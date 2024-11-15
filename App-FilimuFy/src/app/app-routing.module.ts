67dad704de03e1e9b8511abf28572fd5440e0eb3
alvarocastilla05 <castilla.caalv24@triana.salesianos.edu>
2024-11-15 14:16:45 +0100

    Lista peliculas terminado

--- App-FilimuFy/src/app/app-routing.module.ts
+++ App-FilimuFy/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PeliculasListComponent } from './components/peliculas-list/peliculas-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'peliculas', component: PeliculasListComponent},
+  {path: '', redirectTo: 'peliculas', pathMatch: 'full'},
+  /*{path: 'peliculas/:id', component: PeliculasDetailComponent}*/
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
