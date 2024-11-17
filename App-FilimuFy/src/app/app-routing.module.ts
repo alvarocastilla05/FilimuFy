bc481c005bc05e14fb33439816326d164362a991
David Falla Urtiaga <falla.urdav24@triana.salesianos.edu>
2024-11-17 18:47:04 +0100

    Error solucionado

--- App-FilimuFy/src/app/app-routing.module.ts
+++ App-FilimuFy/src/app/app-routing.module.ts
@@ -7,7 +7,7 @@ const routes: Routes = [
   {path: 'peliculas', component: PeliculasListComponent},
   {path: 'series',component: SerieListComponent},
   {path: '', redirectTo: 'peliculas', pathMatch: 'full'}
-
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
