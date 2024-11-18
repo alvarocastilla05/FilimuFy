727151c9d871f7339827ddc421c0935e93eafba9
Daniel Martínez León <daniel1martinezleon@gmail.com>
2024-11-18 12:23:23 +0100

    en ello estamos x2

---
+++ App-FilimuFy/src/app/components/actor-card/actor-card.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input } from '@angular/core';
+import { ActorDetailResponse } from '../../interfaces/actor-detail.interface';
+import { ActorService } from '../../services/actor.service';
+import { Actor, KnownFor } from '../../interfaces/actores-list.interface';
+
+@Component({
+  selector: 'app-actor-card',
+  templateUrl: './actor-card.component.html',
+  styleUrl: './actor-card.component.css'
+})
+export class ActorCardComponent {
+
+  @Input() actorId: Actor | undefined;
+  actorDetalle: Actor | undefined;
+  knownForDisplay: string[] = [];
+
+  constructor(private actorService: ActorService) { }
+
+}
