378ef1d5ec4208796840d4fc89e70eec15d9253a
Daniel Martinez <daniel1martinezleon@gmail.com>
2024-11-21 18:50:40 +0100

    datos en español

--- App-FilimuFy/src/app/services/tvshow.service.ts
+++ App-FilimuFy/src/app/services/tvshow.service.ts
@@ -19,35 +19,35 @@ export class TVShowService {
   constructor(private http: HttpClient) { }
 
   getSeries(page: number = 1): Observable<TVShowListResponse>{
-    return this.http.get<TVShowListResponse>(`https://api.themoviedb.org/3/discover/tv?api_key=${API_KEY}&page=${page}`);
+    return this.http.get<TVShowListResponse>(`https://api.themoviedb.org/3/discover/tv?api_key=${API_KEY}&page=${page}&language=es-ES`);
   }
 
   getSerieById(id: number): Observable<SerieDetailResponse>{
-    return this.http.get<SerieDetailResponse>(`https://api.themoviedb.org/3/tv/${id}?api_key=${API_KEY}`);
+    return this.http.get<SerieDetailResponse>(`https://api.themoviedb.org/3/tv/${id}?api_key=${API_KEY}&language=es-ES`);
   }
 
   getGeneroById(id: number): Observable<TVShowListResponse>{
-    return this.http.get<TVShowListResponse>(`https://api.themoviedb.org/3/genre/tv/list?api_key=${API_KEY}`);
+    return this.http.get<TVShowListResponse>(`https://api.themoviedb.org/3/genre/tv/list?api_key=${API_KEY}&language=es-ES`);
   }
 
   getVideoSerieById(id: string): Observable<VideoSerieListResponse>{
-    return this.http.get<VideoSerieListResponse>(`https://api.themoviedb.org/3/tv/${id}/videos?api_key=${API_KEY}`);
+    return this.http.get<VideoSerieListResponse>(`https://api.themoviedb.org/3/tv/${id}/videos?api_key=${API_KEY}&language=es-ES`);
   }
 
   getCreditosSerieById(id: number): Observable<CreditosListResponse>{
-    return this.http.get<CreditosListResponse>(`https://api.themoviedb.org/3/tv/${id}/credits?api_key=${API_KEY}`);
+    return this.http.get<CreditosListResponse>(`https://api.themoviedb.org/3/tv/${id}/credits?api_key=${API_KEY}&language=es-ES`);
   }
 
   getCertificationById(id: number): Observable<RatingsListResponse>{
-    return this.http.get<RatingsListResponse>(`https://api.themoviedb.org/3/tv/${id}/content_ratings?api_key=${API_KEY}`);
+    return this.http.get<RatingsListResponse>(`https://api.themoviedb.org/3/tv/${id}/content_ratings?api_key=${API_KEY}&language=es-ES`);
   }
 
   getProveedoresById(id: number): Observable<ProveedoreesSerieListResponse>{
-    return this.http.get<ProveedoreesSerieListResponse>(`https://api.themoviedb.org/3/tv/${id}/watch/providers?api_key=${API_KEY}`);
+    return this.http.get<ProveedoreesSerieListResponse>(`https://api.themoviedb.org/3/tv/${id}/watch/providers?api_key=${API_KEY}&language=es-ES`);
   }
 
   getKeywordsById(id: number): Observable<KeywordsSeriesListResponse>{
-    return this.http.get<KeywordsSeriesListResponse>(`https://api.themoviedb.org/3/tv/${id}/keywords?api_key=${API_KEY}`);
+    return this.http.get<KeywordsSeriesListResponse>(`https://api.themoviedb.org/3/tv/${id}/keywords?api_key=${API_KEY}&language=es-ES`);
   }
 
 }
