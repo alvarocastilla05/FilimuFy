57b29a7b003f7cb9d3f70cd89d872bbd20557825
David Falla Urtiaga <falla.urdav24@triana.salesianos.edu>
2024-11-25 10:36:06 +0100

    Servicio favoritos para pelis y series creado

--- App-FilimuFy/src/app/services/autenticacion/account.service.ts
+++ App-FilimuFy/src/app/services/autenticacion/account.service.ts
@@ -2,6 +2,8 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { AccountDetailsResponse } from '../../interfaces/autenticacion/account-details.interface';
+import { PeliculaListResponse } from '../../interfaces/pelicula/pelicula-list.interfaces';
+import { TVShowListResponse } from '../../interfaces/serie/tv.interface';
 
 const API_KEY = "330dac319c12144e2cfd7dfb4bfcb9fd";
 
@@ -18,4 +20,13 @@ export class AccountService {
       `https://api.themoviedb.org/3/account?api_key=${API_KEY}&session_id=${sessionId}`
     );
   }
+
+  getPeliculasFavoritas(page: number = 1, account_id: number | undefined): Observable<PeliculaListResponse>{
+    return this.http.get<PeliculaListResponse>(`https://api.themoviedb.org/3/account/${account_id}/favorite/movies?api_key=${API_KEY}&page=${page}&language=es-ES`);
+  }
+
+  getSeriesFavoritas(page: number = 1, account_id: number | undefined): Observable<TVShowListResponse>{
+    return this.http.get<TVShowListResponse>(`https://api.themoviedb.org/3/account/${account_id}/favorite/tv?api_key=${API_KEY}&page=${page}&language=es-ES`);
+  }
+  
 }
